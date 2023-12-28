c9511e54f52ecbbf802b2a712ad6951e0e3ac172
Michael Kern <m55kern@icloud.com>
2023-12-28 21:11:31 +0100

    Fehlerbehandlung f.d. Liste der Eingaebparameter

--- src/HCat.hs
+++ src/HCat.hs
@@ -1,9 +1,23 @@
 module HCat where
+-- R. Skinner, Kap. 8
 
 import qualified System.Environment as Env
 
 runHCat :: IO ()
-runHCat = handleArgs >>= print
+runHCat = handleArgs >>= displayMessage 
+  where
+    displayMessage parsedArgs =
+      case parsedArgs of
+        Left errorMessage ->
+          putStrLn $ "Error: " <> errorMessage
+        Right filename ->
+          putStrLn $ "Opening file: " <> filename
 
-handleArgs :: IO FilePath
-handleArgs = head <$> Env.getArgs
+handleArgs :: IO (Either String FilePath)
+handleArgs = parseArgs <$> Env.getArgs
+  where
+    parseArgs argumentList =
+      case argumentList of
+        [fname] -> Right fname
+        [] -> Left "Dateiname fehlt"
+        _mehrereArgumente  -> Left "es ist nur ein Dateiname erlaubt"
