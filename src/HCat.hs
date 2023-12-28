3bb70e43cf7e543eb84f7138f35f07f431d2e71a
Michael Kern <m55kern@icloud.com>
2023-12-28 20:29:16 +0100

    erste Versuche mit Eingaben von Parametern

--- src/HCat.hs
+++ src/HCat.hs
@@ -1,4 +1,9 @@
 module HCat where
 
+import qualified System.Environment as Env
+
 runHCat :: IO ()
-runHCat = putStrLn "pager"
+runHCat = handleArgs >>= print
+
+handleArgs :: IO FilePath
+handleArgs = head <$> Env.getArgs
