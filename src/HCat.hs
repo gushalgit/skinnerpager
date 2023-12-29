3b578821d4f8c3546569d3ad1d83423e104a4204
Michael Kern <m55kern@icloud.com>
2023-12-29 16:09:54 +0100

    Fehlerbehandlung nochmals geaendert, Either Werte in Error umgewandelt

--- src/HCat.hs
+++ src/HCat.hs
@@ -1,17 +1,25 @@
-module HCat where
+{-# LANGUAGE TypeApplications #-}
+
+module 
+  HCat (runHCat,)
+where
 -- R. Skinner, Kap. 8
 
+import qualified Control.Exception as Exception
 import qualified System.Environment as Env
+import qualified System.IO.Error as IOError
 
 runHCat :: IO ()
-runHCat = handleArgs >>= displayMessage 
+runHCat =
+  handleIOErr $
+    handleArgs
+    >>= eitherToError
+    >>= readFile
+    >>= putStrLn
   where
-    displayMessage parsedArgs =
-      case parsedArgs of
-        Left errorMessage ->
-          putStrLn $ "Error: " <> errorMessage
-        Right filename ->
-          putStrLn $ "Opening file: " <> filename
+    handleIOErr :: IO () -> IO ()
+    handleIOErr ioAction = Exception.catch ioAction $
+      \e -> putStrLn "ran into an error:" >> print @IOError e
 
 handleArgs :: IO (Either String FilePath)
 handleArgs = parseArgs <$> Env.getArgs
@@ -20,4 +28,9 @@ handleArgs = parseArgs <$> Env.getArgs
       case argumentList of
         [fname] -> Right fname
         [] -> Left "Dateiname fehlt"
-        _mehrereArgumente  -> Left "es ist nur ein Dateiname erlaubt"
+        _mehrereArgumente -> Left "es ist nur ein Dateiname erlaubt"
+
+eitherToError :: Show a => Either a b -> IO b
+eitherToError (Right x) = return x
+eitherToError (Left e) =
+  Exception.throwIO . IOError.userError $ show e
