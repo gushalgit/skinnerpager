ceb32456ce7f87ff92e9c73fdfe982754f449263
Michael Kern <m55kern@icloud.com>
2023-12-29 23:33:10 +0100

    Versuch fuer softwrapping, ist aber noch nicht gelungen

--- src/HCat.hs
+++ src/HCat.hs
@@ -5,6 +5,9 @@ module HCat (runHCat) where
 -- R. Skinner, Kap. 8
 
 import qualified Control.Exception as Exception
+import qualified Data.ByteString as BS
+import qualified Data.Text as Text
+import qualified Data.Text.IO as TextIO
 import qualified System.Environment as Env
 import qualified System.IO.Error as IOError
 
@@ -33,3 +36,40 @@ eitherToError :: (Show a) => Either a b -> IO b
 eitherToError (Right x) = return x
 eitherToError (Left e) =
   Exception.throwIO . IOError.userError $ show e
+
+-- Behandlung von Text als Menge von Zeichen, die auf einer Seite der
+-- Groesse ze x sp dargestellt werden sollen
+
+data ScreenDimension = ScreenDimension
+  { rows :: Int,
+    cols :: Int
+  }
+  deriving (Show)
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf _ [] = []
+groupsOf n elems =
+  let (hd, tl) = splitAt n elems
+   in hd : groupsOf n tl
+
+paginate :: ScreenDimension -> Text.Text -> [Text.Text]
+paginate dim txt =
+  let allLines = Text.lines txt
+      wrappedLines = concatMap (softWrap (cols dim)) allLines
+      pageLines = groupsOf (rows dim) wrappedLines
+   in map Text.unlines pageLines
+
+softWrap :: Int -> Text.Text -> [Text.Text]
+softWrap maxlg line
+  | Text.length line <= maxlg = [line]
+  | otherwise =
+      let (candidate, rest) = Text.splitAt maxlg line
+          (firstPart, overflow) = splitOnFirstSpace candidate (Text.length candidate - 1)
+       in firstPart : softWrap maxlg (overflow <> rest)
+  where
+    splitOnFirstSpace cand textIdx
+      | textIdx <= 0 = (cand, Text.empty)
+      | Text.index cand textIdx == ' ' =
+          let (wrappedLine, rest) = Text.splitAt textIdx cand
+           in (wrappedLine, Text.tail rest)
+      | otherwise = splitOnFirstSpace cand (textIdx - 1)
